b9c13856ae0fa2a49c10874335a24b9ac1680786
Sainathreddy <sai65265@gmail.com>
2026-02-18 01:33:50 +0530

    feat: Add JsonPreview component using jsoneditor-react with custom styling and type definitions.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,73 +1,40 @@
 declare module "react-json-view" {
   import React from "react";
+  // ... (previous)
+  const ReactJson: React.FC<any>;
+  export default ReactJson;
+}
 
-  export interface ReactJsonProps {
-    src: object | null;
-    name?: string | false | null;
+declare module "jsoneditor-react" {
+  import { Component } from "react";
+  import { JSONEditorOptions } from "jsoneditor";
+
+  export interface JsonEditorProps {
+    value?: any;
+    mode?: "tree" | "view" | "form" | "code" | "text";
+    name?: string;
+    schema?: object;
+    schemaRefs?: object;
+    onChange?: (value: any) => void;
+    onChangeJSON?: (value: any) => void;
+    onChangeText?: (value: string) => void;
+    onModeChange?: (newMode: string, oldMode: string) => void;
+    onValidate?: (json: any) => any;
+    onError?: (err: Error) => void;
+    ace?: object;
+    ajv?: object;
     theme?: string;
-    style?: object;
-    iconStyle?: "circle" | "triangle" | "square";
-    indentWidth?: number;
-    collapsed?: boolean | number;
-    collapseStringsAfterLength?: number | false;
-    shouldCollapse?:
-      | ((field: {
-          name: string;
-          src: object;
-          type: string;
-          namespace: Array<string | null>;
-        }) => boolean)
-      | false;
-    sortKeys?: boolean;
-    quotesOnKeys?: boolean;
-    groupArraysAfterLength?: number | false;
-    enableClipboard?:
-      | boolean
-      | ((copy: {
-          src: object;
-          name: string;
-          namespace: Array<string | null>;
-        }) => void);
-    displayObjectSize?: boolean;
-    displayDataTypes?: boolean;
-    onEdit?:
-      | ((edit: {
-          updated_src: object;
-          name: string;
-          namespace: Array<string | null>;
-          new_value?: any;
-          existing_value?: any;
-        }) => void)
-      | false;
-    onAdd?:
-      | ((add: {
-          updated_src: object;
-          name: string;
-          namespace: Array<string | null>;
-          new_value?: any;
-          existing_value?: any;
-        }) => void)
-      | false;
-    onDelete?:
-      | ((del: {
-          updated_src: object;
-          name: string;
-          namespace: Array<string | null>;
-          new_value?: any;
-          existing_value?: any;
-        }) => void)
-      | false;
-    onSelect?:
-      | ((select: {
-          name: string;
-          value: any;
-          namespace: Array<string | null>;
-        }) => void)
-      | false;
-    validationMessage?: string;
-    defaultValue?: object | null;
+    history?: boolean;
+    navigationBar?: boolean;
+    statusBar?: boolean;
+    search?: boolean;
+    allowedModes?: string[];
+    tag?: string;
+    htmlElementProps?: object | React.HTMLAttributes<HTMLElement>;
+    innerRef?: (instance: any) => void;
+    sortObjectKeys?: boolean;
+    mainMenuBar?: boolean;
   }
 
-  const ReactJson: React.FC<ReactJsonProps>;
-  export default ReactJson;
+  export class JsonEditor extends Component<JsonEditorProps> {}
 }
